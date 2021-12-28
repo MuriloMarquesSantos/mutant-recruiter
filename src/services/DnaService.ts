7ad24d65847afc5d3461be7dad24be3ee4dbdadb
MuriloMarquesSantos <murilommms@gmail.com>
2021-12-28 12:09:30 -0300

    Adding two first passing test cases

--- src/services/DnaService.ts
+++ src/services/DnaService.ts
@@ -1,3 +1,21 @@
 export default class DnaService {
-    
+    isMutant(testDna: string[]): boolean {
+        let dnaMatchLetterCounter = 0;
+        let dnaSequenceMatchCounter = 0;
+        const joinDnaSequence = testDna.join('');
+        for (let index = 1; index < joinDnaSequence.length; index++) {
+            if (joinDnaSequence[index] === joinDnaSequence[index - 1]) {
+                dnaMatchLetterCounter++;
+            } else {
+                dnaMatchLetterCounter = 0;
+            }
+            if (dnaMatchLetterCounter === 3) {
+                dnaSequenceMatchCounter++;
+            }
+            if (dnaSequenceMatchCounter === 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- test/DnaService.test.ts
+++ test/DnaService.test.ts
@@ -1,11 +1,19 @@
 import DnaService from '../src/services/DnaService';
 
-describe('DnaService', () => [
-    it('should detect a mutant given a valid DNA', () => {
-        const testDna = ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"];
+describe('DnaService', () => {
+    it('should detect a mutant given a DNA that contains multiple horizontal sequences', () => {
+        const testDna = ["CTTTTA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"];
         const dnaService = new DnaService();
         const isMutantDna = dnaService.isMutant(testDna);
 
         expect(isMutantDna).toBe(true);
+    });
+
+    it('should not detect a mutant given a DNA that contains only one horizontal sequence', () => {
+        const testDna = ["CTTTTA","CAGTGC","TTATGT","AGAAGG","CCCATA","TCACTG"];
+        const dnaService = new DnaService();
+        const isMutantDna = dnaService.isMutant(testDna);
+
+        expect(isMutantDna).toBe(false); 
     })
-])
+});
