a60853d1e5bfab1d3454127c01bc6af5329e178c
MuriloMarquesSantos <murilommms@gmail.com>
2021-12-28 12:52:56 -0300

    Refactoring analyzeDnaHorizontally function

--- src/services/DnaService.ts
+++ src/services/DnaService.ts
@@ -1,21 +1,35 @@
 export default class DnaService {
     isMutant(testDna: string[]): boolean {
+        const joinDnaSequence = testDna.join('');
+        return this.analyzeDnaHorizontally(joinDnaSequence);
+    }
+
+    private analyzeDnaHorizontally(joinDnaSequence: string): boolean {
+        const MATCH_DNA_LETTER_TARGET = 3;
+        const MATCH_DNA_SEQUENCE_TARGET = 2;
         let dnaMatchLetterCounter = 0;
         let dnaSequenceMatchCounter = 0;
-        const joinDnaSequence = testDna.join('');
+
         for (let index = 1; index < joinDnaSequence.length; index++) {
-            if (joinDnaSequence[index] === joinDnaSequence[index - 1]) {
-                dnaMatchLetterCounter++;
-            } else {
-                dnaMatchLetterCounter = 0;
-            }
-            if (dnaMatchLetterCounter === 3) {
+            const currentDnaLetter = joinDnaSequence[index];
+            const previousDnaLetter = joinDnaSequence[index - 1];
+
+            dnaMatchLetterCounter = this.retrieveDnaLetterMatch(currentDnaLetter, previousDnaLetter, dnaMatchLetterCounter); 
+            
+            if (dnaMatchLetterCounter === MATCH_DNA_LETTER_TARGET) {
                 dnaSequenceMatchCounter++;
             }
-            if (dnaSequenceMatchCounter === 2) {
+            if (dnaSequenceMatchCounter === MATCH_DNA_SEQUENCE_TARGET) {
                 return true;
             }
         }
         return false;
+    }
+
+    private retrieveDnaLetterMatch(currentDnaLetter: string, previousDnaLetter:string, dnaMatchLetterCounter: number): number {
+        if (currentDnaLetter === previousDnaLetter) {
+            return dnaMatchLetterCounter + 1;
+        }
+        return 0;
     }
 }
