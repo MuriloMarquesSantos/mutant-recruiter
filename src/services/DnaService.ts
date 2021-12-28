061c769cce62775f14e415bda09f805df34e5024
MuriloMarquesSantos <murilommms@gmail.com>
2021-12-28 16:27:09 -0300

    Adding test cases and implementation for vertical search

--- src/services/DnaService.ts
+++ src/services/DnaService.ts
@@ -1,12 +1,21 @@
 export default class DnaService {
+    MATCH_DNA_SEQUENCE_TARGET = 2;
+    MATCH_DNA_LETTER_TARGET = 3;
     isMutant(testDna: string[]): boolean {
+        let dnaMatchCounter = 0;
         const joinDnaSequence = testDna.join('');
-        return this.analyzeDnaHorizontally(joinDnaSequence);
+        const horizontalMatchCounter = this.analyzeDnaHorizontally(joinDnaSequence);
+        const verticalMatchCounter = this.analyzeDnaVertically(testDna);
+
+        dnaMatchCounter = horizontalMatchCounter + verticalMatchCounter;
+
+        if (dnaMatchCounter >= this.MATCH_DNA_SEQUENCE_TARGET) {
+            return true;
+        }
+        return false;
     }
 
-    private analyzeDnaHorizontally(joinDnaSequence: string): boolean {
-        const MATCH_DNA_LETTER_TARGET = 3;
-        const MATCH_DNA_SEQUENCE_TARGET = 2;
+    private analyzeDnaHorizontally(joinDnaSequence: string): number {
         let dnaMatchLetterCounter = 0;
         let dnaSequenceMatchCounter = 0;
 
@@ -16,20 +25,50 @@ export default class DnaService {
 
             dnaMatchLetterCounter = this.retrieveDnaLetterMatch(currentDnaLetter, previousDnaLetter, dnaMatchLetterCounter); 
             
-            if (dnaMatchLetterCounter === MATCH_DNA_LETTER_TARGET) {
+            if (dnaMatchLetterCounter === this.MATCH_DNA_LETTER_TARGET) {
                 dnaSequenceMatchCounter++;
             }
-            if (dnaSequenceMatchCounter === MATCH_DNA_SEQUENCE_TARGET) {
-                return true;
+            if (dnaSequenceMatchCounter === this.MATCH_DNA_SEQUENCE_TARGET) {
+                return this.MATCH_DNA_SEQUENCE_TARGET;
             }
         }
-        return false;
+        return dnaSequenceMatchCounter;
     }
 
     private retrieveDnaLetterMatch(currentDnaLetter: string, previousDnaLetter:string, dnaMatchLetterCounter: number): number {
         if (currentDnaLetter === previousDnaLetter) {
             return dnaMatchLetterCounter + 1;
         }
+        return 0;
+    }
+
+    private analyzeDnaVertically(testDna: string[]): number {
+        //candidates
+        // O (N) -> Leitura de cada uma das verticais. N + Length a cada rodada
+        // Prestar atenção nos limites verticais
+        const dnaSequenceRowLength = testDna[0].length;
+        const joinDnaSequence = testDna.join('');
+        const dnaSequenceDepth = (dnaSequenceRowLength * dnaSequenceRowLength);
+        let dnaMatchLetterCounter = 0;
+        let dnaSequenceMatchCounter = 0; 
+
+        for (let index = 0; index < dnaSequenceRowLength; index++) {
+            for (let index2 = index + dnaSequenceRowLength; index2 <= dnaSequenceDepth + index; index2+=dnaSequenceRowLength) {
+                const currentDnaLetter = joinDnaSequence[index2];
+                const previousDnaLetter = joinDnaSequence[index2 - dnaSequenceRowLength]; 
+
+                dnaMatchLetterCounter = this.retrieveDnaLetterMatch(currentDnaLetter, previousDnaLetter, dnaMatchLetterCounter); 
+
+                if (dnaMatchLetterCounter === this.MATCH_DNA_LETTER_TARGET) {
+                    dnaSequenceMatchCounter++;
+                }
+                if (dnaSequenceMatchCounter === this.MATCH_DNA_SEQUENCE_TARGET) {
+                    return this.MATCH_DNA_SEQUENCE_TARGET;
+                }
+            }
+            dnaMatchLetterCounter = 0;
+        }
+
         return 0;
     }
 }
--- test/DnaService.test.ts
+++ test/DnaService.test.ts
@@ -1,7 +1,7 @@
 import DnaService from '../src/services/DnaService';
 
 describe('DnaService', () => {
-    it('should detect a mutant given a DNA that contains multiple horizontal sequences', () => {
+    it('should detect a mutant given a DNA that contains multiple horizontal matches', () => {
         const testDna = ["CTTTTA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"];
         const dnaService = new DnaService();
         const isMutantDna = dnaService.isMutant(testDna);
@@ -9,11 +9,35 @@ describe('DnaService', () => {
         expect(isMutantDna).toBe(true);
     });
 
-    it('should not detect a mutant given a DNA that contains only one horizontal sequence', () => {
+    it('should not detect a mutant given a DNA that contains only one horizontal match', () => {
         const testDna = ["CTTTTA","CAGTGC","TTATGT","AGAAGG","CCCATA","TCACTG"];
         const dnaService = new DnaService();
         const isMutantDna = dnaService.isMutant(testDna);
 
         expect(isMutantDna).toBe(false); 
-    })
+    });
+
+    it('should not detect a mutant given a DNA that does not contain any valid match', () => {
+        const testDna = ["CTTAA","CAGTGC","TTATGT","AGAAGG","CCCATA","TCACTG"];
+        const dnaService = new DnaService();
+        const isMutantDna = dnaService.isMutant(testDna);
+
+        expect(isMutantDna).toBe(false); 
+    });
+
+    it('should detect a mutant given a DNA that contains multiple vertical matches', () => {
+        const testDna = ["ATGCGA","CAATGC","TTATGT","AGACGA","CCAATC","TCACTT"];
+        const dnaService = new DnaService();
+        const isMutantDna = dnaService.isMutant(testDna);
+
+        expect(isMutantDna).toBe(true); 
+    });
+
+    it('should not detect a mutant given a DNA that contains only one vertical match', () => {
+        const testDna = ["ATGCTA","CAATGC","TTATGT","AGACGA","CCAATC","TCACTT"];
+        const dnaService = new DnaService();
+        const isMutantDna = dnaService.isMutant(testDna);
+
+        expect(isMutantDna).toBe(false); 
+    });
 });
